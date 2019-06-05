2c87361e2ef8dc18e6c9abc805b5ae739e1572d1
Waleed AlMalki <wm.wexem@gmail.com>
2019-06-05 12:41:15 +0300

    Got rid of method middleware.

--- generator/service_middleware.go
+++ generator/service_middleware.go
@@ -1,28 +1,25 @@
 package generator
 
 import (
-	"github.com/wlMalk/goms/generator/strings"
 	"github.com/wlMalk/goms/parser/types"
 )
 
 func generateServiceMiddlewareFile(base string, path string, name string, service *types.Service) *GoFile {
 	file := NewGoFile(base, path, name, true, false)
 	generateServiceMiddlewareType(file, service)
-	for _, method := range service.Methods {
-		generateMethodMiddlewareType(file, method)
-	}
 	return file
 }
 
 func generateServiceMiddlewareType(file *GoFile, service *types.Service) {
 	file.AddImport("", service.ImportPath, "/service/handlers")
-	file.Pf("type Middleware func(handlers.RequestResponseHandler) handlers.RequestResponseHandler")
-	file.Pf("")
-}
-
-func generateMethodMiddlewareType(file *GoFile, method *types.Method) {
-	file.AddImport("", method.Service.ImportPath, "/service/handlers")
-	methodName := strings.ToUpperFirst(method.Name)
-	file.Pf("type %sMiddleware func(handlers.%sRequestResponseHandler) handlers.%sRequestResponseHandler", methodName, methodName, methodName)
-	file.Pf("")
+	file.P("type Middleware func(handlers.RequestResponseHandler) handlers.RequestResponseHandler")
+	file.P("")
+	file.P("func Chain(outer Middleware, others ...Middleware) Middleware {")
+	file.P("return func(next handlers.RequestResponseHandler) handlers.RequestResponseHandler {")
+	file.P("for i := len(others) - 1; i >= 0; i-- {")
+	file.P("next = others[i](next)")
+	file.P("}")
+	file.P("return outer(next)")
+	file.P("}")
+	file.P("}")
 }
