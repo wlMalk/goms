a0011b3de6f22c732625048bdc0f702fc4c8a4fe
Waleed AlMalki <wm.wexem@gmail.com>
2019-06-05 17:22:55 +0300

    Defined common encoder for http responses.

--- goms/http/http.go
+++ goms/http/http.go
@@ -6,6 +6,11 @@ import (
 	"net/http"
 )
 
+func HTTPResponseEncoder(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	return json.NewEncoder(w).Encode(response)
+}
+
 type Router interface {
 	Method(method string, uri string, handler http.Handler)
 	PathParams(req *http.Request) PathParams
