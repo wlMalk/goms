a83af30b0b748b8b34c0244754402f85ac7adcd2
Waleed AlMalki <wm.wexem@gmail.com>
2019-06-06 15:13:21 +0300

    Added generators for HTTP response decoders.

---
+++ generator/http_decoders.go
@@ -0,0 +1,38 @@
+package generator
+
+import (
+	"github.com/wlMalk/goms/generator/strings"
+	"github.com/wlMalk/goms/parser/types"
+)
+
+func generateHTTPDecodersFile(base string, path string, name string, service *types.Service) *GoFile {
+	file := NewGoFile(base, path, name, true, false)
+	for _, method := range service.Methods {
+		generateHTTPRequestDecoder(file, method)
+		generateHTTPResponseDecoder(file, method)
+	}
+	return file
+}
+
+func generateHTTPRequestDecoder(file *GoFile, method *types.Method) {
+
+}
+
+func generateHTTPResponseDecoder(file *GoFile, method *types.Method) {
+	file.AddImport("", "context")
+	file.AddImport("", "net/http")
+	methodName := strings.ToUpperFirst(method.Name)
+	file.Pf("func Decode%sResponse(ctx context.Context, res *http.Response) (interface{}, error) {", methodName)
+	if len(method.Results) > 0 {
+		file.AddImport("", method.Service.ImportPath, "service/transport/http/responses")
+		file.Pf("resp, err := responses.%sHTTP(res)", methodName)
+		file.Pf("if err!=nil{")
+		file.Pf("return nil, err")
+		file.Pf("}")
+		file.Pf("return resp.Response(), err")
+	} else {
+		file.Pf("return nil, nil")
+	}
+	file.Pf("}")
+	file.Pf("")
+}
