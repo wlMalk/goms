a54aa9910c1a77377902c04e910a7e8763653dba
Waleed AlMalki <wm.wexem@gmail.com>
2019-06-07 03:30:08 +0300

    Added getMethodURI to calculate method URI.

--- generator/http_server.go
+++ generator/http_server.go
@@ -9,40 +9,57 @@ import (
 
 func generateHTTPServerFile(base string, path string, name string, service *types.Service) *GoFile {
 	file := NewGoFile(base, path, name, true, false)
-	generateHTTPTransportServerInitFunc(file, service)
+	generateHTTPTransportServerStruct(file, service)
+	generateHTTPTransportServerNewFunc(file, service)
 	return file
 }
 
-func generateHTTPTransportServerInitFunc(file *GoFile, service *types.Service) {
+func generateHTTPTransportServerStruct(file *GoFile, service *types.Service) {
+	file.Pf("type Server struct {")
+	file.Pf("http.Server")
+	file.Pf("}")
+	file.Pf("")
+}
+
+func generateHTTPTransportServerNewFunc(file *GoFile, service *types.Service) {
 	serviceName := strings.ToUpperFirst(service.Name)
 	serviceNameSnake := strings.ToSnakeCase(service.Name)
+	file.AddImport("", "net/http")
 	file.AddImport("kit_http", "github.com/go-kit/kit/transport/http")
 	file.AddImport("", service.ImportPath, "/service/transport")
 	file.AddImport(serviceNameSnake+"_http", service.ImportPath, "/service/transport/http")
 	file.AddImport("goms_http", "github.com/wlMalk/goms/goms/http")
-	file.Pf("func Init(router goms_http.Router, endpoints *transport.%s, opts ...kit_http.ServerOption) {", serviceName)
-	serviceVersion := "v" + service.Version.String()
-	serviceHTTPURIPrefix := service.Options.HTTP.URIPrefix
-	if serviceHTTPURIPrefix == "" {
-		serviceHTTPURIPrefix = serviceNameSnake
-	}
+	file.Pf("func New(router goms_http.Router, endpoints *transport.%s, opts ...kit_http.ServerOption) *Server {", serviceName)
 	for _, method := range service.Methods {
 		methodName := strings.ToUpperFirst(method.Name)
-		methodNameSnake := strings.ToSnakeCase(method.Name)
 		methodHTTPMethod := method.Options.HTTP.Method
-		methodHTTPURI := method.Options.HTTP.URI
-		methodHTTPabsURI := method.Options.HTTP.AbsURI
-		if methodHTTPabsURI == "" {
-			if methodHTTPURI == "" {
-				methodHTTPURI = methodNameSnake
-			}
-			methodHTTPabsURI = path.Join(serviceHTTPURIPrefix, methodHTTPURI)
-		}
-		file.Pf("router.Method(\"%s\", \"/%s\", kit_http.NewServer(", methodHTTPMethod, path.Join(serviceVersion, methodHTTPabsURI))
+		methodURI := getMethodURI(method)
+		file.Pf("router.Method(\"%s\", \"/%s\", kit_http.NewServer(", methodHTTPMethod, methodURI)
 		file.Pf("endpoints.%s,", methodName)
 		file.Pf("%s_http.Decode%sRequest,", serviceNameSnake, methodName)
 		file.Pf("%s_http.Encode%sResponse,", serviceNameSnake, methodName)
 		file.Pf("opts...))")
 	}
+	file.Pf("return &Server{Server: http.Server{Handler: router}}")
 	file.Pf("}")
+	file.Pf("")
+}
+
+func getMethodURI(method *types.Method) string {
+	serviceNameSnake := strings.ToSnakeCase(method.Service.Name)
+	serviceVersion := "v" + method.Service.Version.String()
+	serviceHTTPURIPrefix := method.Service.Options.HTTP.URIPrefix
+	if serviceHTTPURIPrefix == "" {
+		serviceHTTPURIPrefix = serviceNameSnake
+	}
+	methodNameSnake := strings.ToSnakeCase(method.Name)
+	methodHTTPURI := method.Options.HTTP.URI
+	methodHTTPabsURI := method.Options.HTTP.AbsURI
+	if methodHTTPabsURI == "" {
+		if methodHTTPURI == "" {
+			methodHTTPURI = methodNameSnake
+		}
+		methodHTTPabsURI = path.Join(serviceHTTPURIPrefix, methodHTTPURI)
+	}
+	return path.Join(serviceVersion, methodHTTPabsURI)
 }
