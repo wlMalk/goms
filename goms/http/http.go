9c7b3416fec02ed8d93ed02ff4727f93b5d2b21f
Waleed AlMalki <wm.wexem@gmail.com>
2019-06-07 07:46:16 +0300

    Added getter and setter for router params in context.

--- goms/http/http.go
+++ goms/http/http.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 func HTTPResponseEncoder(_ context.Context, w http.ResponseWriter, response interface{}) error {
@@ -13,10 +14,22 @@ func HTTPResponseEncoder(_ context.Context, w http.ResponseWriter, response inte
 
 type Router interface {
 	Method(method string, uri string, handler http.Handler)
-	Params(req *http.Request) Params
 	ServeHTTP(w http.ResponseWriter, r *http.Request)
 }
 
 type Params interface {
 	Get(name string) string
 }
+
+type contextKey int
+
+const ContextParamsKey contextKey = iota
+
+func SetParams(ctx context.Context, params Params) context.Context {
+	return context.WithValue(ctx, ContextParamsKey, params)
+}
+
+func GetParams(ctx context.Context) Params {
+	return ctx.Value(ContextParamsKey).(Params)
+}
+
