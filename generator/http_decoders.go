f5ebc722e18d58faba011c9b7834016b67facc5b
Waleed AlMalki <wm.wexem@gmail.com>
2019-06-07 03:28:57 +0300

    Added generator for http request decoder.

--- generator/http_decoders.go
+++ generator/http_decoders.go
@@ -15,7 +15,22 @@ func generateHTTPDecodersFile(base string, path string, name string, service *ty
 }
 
 func generateHTTPRequestDecoder(file *GoFile, method *types.Method) {
-
+	file.AddImport("", "context")
+	file.AddImport("", "net/http")
+	methodName := strings.ToUpperFirst(method.Name)
+	file.Pf("func Decode%sRequest(ctx context.Context, req *http.Request) (interface{}, error) {", methodName)
+	if len(method.Results) > 0 {
+		file.AddImport("", method.Service.ImportPath, "service/transport/http/requests")
+		file.Pf("r, err := requests.%sFromHTTP(req)", methodName)
+		file.Pf("if err!=nil{")
+		file.Pf("return nil, err")
+		file.Pf("}")
+		file.Pf("return r.Request(), err")
+	} else {
+		file.Pf("return nil, nil")
+	}
+	file.Pf("}")
+	file.Pf("")
 }
 
 func generateHTTPResponseDecoder(file *GoFile, method *types.Method) {
@@ -25,7 +40,7 @@ func generateHTTPResponseDecoder(file *GoFile, method *types.Method) {
 	file.Pf("func Decode%sResponse(ctx context.Context, res *http.Response) (interface{}, error) {", methodName)
 	if len(method.Results) > 0 {
 		file.AddImport("", method.Service.ImportPath, "service/transport/http/responses")
-		file.Pf("resp, err := responses.%sHTTP(res)", methodName)
+		file.Pf("resp, err := responses.%sFromHTTP(res)", methodName)
 		file.Pf("if err!=nil{")
 		file.Pf("return nil, err")
 		file.Pf("}")
