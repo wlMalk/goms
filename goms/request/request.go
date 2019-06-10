91fb196a4e070b93630a6389a533c514cb1af912
Waleed AlMalki <wm.wexem@gmail.com>
2019-06-10 21:37:42 +0300

    Edited request package.

--- goms/request/request.go
+++ goms/request/request.go
@@ -2,11 +2,20 @@ package request
 
 import (
 	"context"
+
+	"github.com/rs/xid"
 )
 
 type contextKeyType string
 
-const contextRequestIDKey contextKeyType = "request-id"
+const (
+	contextRequestIDKey       contextKeyType = "request-id"
+	contextCallerRequestIDKey contextKeyType = "caller-request-id"
+)
+
+func NewRequestID() string {
+	return xid.New().String()
+}
 
 func SetRequestID(ctx context.Context, requestID string) context.Context {
 	return context.WithValue(ctx, contextRequestIDKey, requestID)
@@ -14,6 +23,18 @@ func SetRequestID(ctx context.Context, requestID string) context.Context {
 
 func GetRequestID(ctx context.Context) string {
 	requestID := ctx.Value(contextRequestIDKey)
+	if requestID == nil {
+		return ""
+	}
+	return requestID.(string)
+}
+
+func SetCallerRequestID(ctx context.Context, requestID string) context.Context {
+	return context.WithValue(ctx, contextCallerRequestIDKey, requestID)
+}
+
+func GetCallerRequestID(ctx context.Context) string {
+	requestID := ctx.Value(contextCallerRequestIDKey)
 	if requestID == nil {
 		return ""
 	}
