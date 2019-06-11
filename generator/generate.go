c824744222c1fb18da980ce2c94bc9486f81b674
Waleed AlMalki <wm.wexem@gmail.com>
2019-06-11 18:53:41 +0300

    Added generated files to files set.

--- generator/generate.go
+++ generator/generate.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/wlMalk/goms/generator/files"
 	"github.com/wlMalk/goms/generator/generate_service"
+	"github.com/wlMalk/goms/generator/helpers"
 	"github.com/wlMalk/goms/generator/strings"
 	"github.com/wlMalk/goms/parser/types"
 )
@@ -13,8 +14,12 @@ func GenerateService(s *types.Service) (files files.Files, err error) {
 	files = append(files, generate_service.GenerateRequestsFile(s.Path, filepath.Join("service", "requests"), "requests.goms", s.Methods))
 	files = append(files, generate_service.GenerateResponseFile(s.Path, filepath.Join("service", "responses"), "responses.goms", s.Methods))
 	files = append(files, generate_service.GenerateHandlersFile(s.Path, filepath.Join("service", "handlers"), "handlers.goms", s))
-	files = append(files, generate_service.GenerateConvertersFile(s.Path, filepath.Join("service", "handlers", "converters"), "converters.goms", s.Methods))
+	files = append(files, generate_service.GenerateConvertersFile(s.Path, filepath.Join("service", "handlers", "converters"), "converters.goms", s))
 	files = append(files, generate_service.GenerateServiceMiddlewareFile(s.Path, filepath.Join("service", "middleware"), "middleware.goms", s))
+	if helpers.IsCachingEnabled(s) {
+		files = append(files, generate_service.GenerateCachingMiddlewareFile(s.Path, filepath.Join("service", "middleware"), "caching_middleware.goms", s))
+		files = append(files, generate_service.GenerateCachingKeyerFile(s.Path, filepath.Join(s.Path, strings.ToLowerFirst(s.Name)), "caching_keyer", s))
+	}
 	files = append(files, generate_service.GenerateServiceTransportEndpointsFile(s.Path, filepath.Join("service", "transport"), "endpoints.goms", s))
 	files = append(files, generate_service.GenerateServiceImplementationFile(s.Path, strings.ToLowerFirst(s.Name), strings.ToLowerFirst(s.Name), s))
 	files = append(files, generate_service.GenerateServiceImplementationValidatorsFile(s.Path, strings.ToLowerFirst(s.Name), "validators", s))
@@ -25,6 +30,7 @@ func GenerateService(s *types.Service) (files files.Files, err error) {
 	files = append(files, generate_service.GenerateHTTPClientFile(s.Path, filepath.Join("service", "transport", "http", "client"), "client.goms", s))
 	files = append(files, generate_service.GenerateHTTPDecodersFile(s.Path, filepath.Join("service", "transport", "http"), "decoders.goms", s))
 	files = append(files, generate_service.GenerateHTTPEncodersFile(s.Path, filepath.Join("service", "transport", "http"), "encoders.goms", s))
+	files = append(files, generate_service.GenerateServiceMainFile(s.Path, filepath.Join("cmd", strings.ToLowerFirst(s.Name)), "main", s))
 
 	return
 }
