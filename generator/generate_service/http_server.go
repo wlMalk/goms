2baff19ad730136489a32c494526b4342e142cd0
Waleed AlMalki <wm.wexem@gmail.com>
2019-06-11 18:39:31 +0300

    Edited http server generator.

--- generator/generate_service/http_server.go
+++ generator/generate_service/http_server.go
@@ -10,38 +10,53 @@ import (
 
 func GenerateHTTPServerFile(base string, path string, name string, service *types.Service) *files.GoFile {
 	file := files.NewGoFile(base, path, name, true, false)
-	generateHTTPTransportServerStruct(file, service)
-	generateHTTPTransportServerNewFunc(file, service)
+	generateHTTPTransportServerRegisterFunc(file, service)
+	generateHTTPTransportServerRegisterSpecialFunc(file, service)
 	return file
 }
 
-func generateHTTPTransportServerStruct(file *files.GoFile, service *types.Service) {
-	file.Pf("type Server struct {")
-	file.Pf("http.Server")
-	file.Pf("}")
-	file.Pf("")
-}
-
-func generateHTTPTransportServerNewFunc(file *files.GoFile, service *types.Service) {
+func generateHTTPTransportServerRegisterFunc(file *files.GoFile, service *types.Service) {
 	serviceName := strings.ToUpperFirst(service.Name)
 	serviceNameSnake := strings.ToSnakeCase(service.Name)
-	file.AddImport("", "net/http")
 	file.AddImport("kit_http", "github.com/go-kit/kit/transport/http")
 	file.AddImport("", service.ImportPath, "/service/transport")
 	file.AddImport(serviceNameSnake+"_http", service.ImportPath, "/service/transport/http")
-	file.AddImport("goms_http", "github.com/wlMalk/goms/goms/http")
-	file.Pf("func New(router goms_http.Router, endpoints *transport.%s, opts ...kit_http.ServerOption) *Server {", serviceName)
+	file.AddImport("goms_http", "github.com/wlMalk/goms/goms/transport/http")
+	file.Pf("func Register(server *goms_http.Server, endpoints *transport.%s, opts ...kit_http.ServerOption) {", serviceName)
 	for _, method := range service.Methods {
 		methodName := strings.ToUpperFirst(method.Name)
 		methodHTTPMethod := method.Options.HTTP.Method
 		methodURI := getMethodURI(method)
-		file.Pf("router.Method(\"%s\", \"%s\", kit_http.NewServer(", methodHTTPMethod, methodURI)
+		file.Pf("server.RegisterMethod(\"%s\", \"%s\", kit_http.NewServer(", methodHTTPMethod, methodURI)
 		file.Pf("endpoints.%s,", methodName)
 		file.Pf("%s_http.Decode%sRequest,", serviceNameSnake, methodName)
 		file.Pf("%s_http.Encode%sResponse,", serviceNameSnake, methodName)
-		file.Pf("opts...))")
+		file.Pf("opts...),")
+		file.Pf(")")
 	}
-	file.Pf("return &Server{Server: http.Server{Handler: router}}")
+	file.Pf("}")
+	file.Pf("")
+}
+
+func generateHTTPTransportServerRegisterSpecialFunc(file *files.GoFile, service *types.Service) {
+	serviceName := strings.ToUpperFirst(service.Name)
+	serviceNameSnake := strings.ToSnakeCase(service.Name)
+	file.AddImport("kit_http", "github.com/go-kit/kit/transport/http")
+	file.AddImport("", service.ImportPath, "/service/transport")
+	file.AddImport(serviceNameSnake+"_http", service.ImportPath, "/service/transport/http")
+	file.AddImport("goms_http", "github.com/wlMalk/goms/goms/transport/http")
+	file.Pf("func RegisterSpecial(server *goms_http.Server, endpoints *transport.%s, optionsFunc func(method string) (opts []kit_http.ServerOption)) {", serviceName)
+	for _, method := range service.Methods {
+		methodName := strings.ToUpperFirst(method.Name)
+		methodHTTPMethod := method.Options.HTTP.Method
+		methodURI := getMethodURI(method)
+		file.Pf("server.RegisterMethod(\"%s\", \"%s\", kit_http.NewServer(", methodHTTPMethod, methodURI)
+		file.Pf("endpoints.%s,", methodName)
+		file.Pf("%s_http.Decode%sRequest,", serviceNameSnake, methodName)
+		file.Pf("%s_http.Encode%sResponse,", serviceNameSnake, methodName)
+		file.Pf("optionsFunc(\"%s\")...),", methodName)
+		file.Pf(")")
+	}
 	file.Pf("}")
 	file.Pf("")
 }
