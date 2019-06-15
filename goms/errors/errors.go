0d6174f80914252a51673e0c612c7ab51d6fdd88
Waleed AlMalki <wm.wexem@gmail.com>
2019-06-15 17:02:14 +0300

    Edited errors package andd added more error types.

--- goms/errors/errors.go
+++ goms/errors/errors.go
@@ -2,17 +2,49 @@ package errors
 
 import "fmt"
 
-type MethodNotImplemented struct {
+type ErrMethodNotImplemented struct {
 	Service string
 	Method  string
 }
 
-func (err *MethodNotImplemented) Error() string {
-	return fmt.Sprintf("method \"%s\" is not implemented in service \"%s\"", err.Method, err.Service)
+func (err *ErrMethodNotImplemented) Error() string {
+	return fmt.Sprintf("method '%s' is not implemented in service '%s'", err.Method, err.Service)
 }
 
-func ErrMethodNotImplemented(service string, method string) error {
-	return &MethodNotImplemented{
+type ErrInvalidRequest struct {
+	Service string
+	Method  string
+}
+
+func (err *ErrInvalidRequest) Error() string {
+	return fmt.Sprintf("invalid request received for '%s' method in '%s' service", err.Method, err.Service)
+}
+
+type ErrInvalidResponse struct {
+	Service string
+	Method  string
+}
+
+func (err *ErrInvalidResponse) Error() string {
+	return fmt.Sprintf("invalid response returned from '%s' method in '%s' service", err.Method, err.Service)
+}
+
+func MethodNotImplemented(service string, method string) error {
+	return &ErrMethodNotImplemented{
+		Service: service,
+		Method:  method,
+	}
+}
+
+func InvalidRequest(service string, method string) error {
+	return &ErrInvalidRequest{
+		Service: service,
+		Method:  method,
+	}
+}
+
+func InvalidResponse(service string, method string) error {
+	return &ErrInvalidResponse{
 		Service: service,
 		Method:  method,
 	}
